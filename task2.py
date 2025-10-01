4a8e3746958159ec57d095b407c86fd639ad97f3
GhulamMustafa4578 <281131380@formanite.fccollege.edu.pk>
2025-10-01 15:31:19 +0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,24 @@
+#Part2
+class Queue():
+    def __init__(self):
+        self.ist=[]
+    def enqueue(self,k):
+        self.ist.append(k)
+    def dequeue(self):
+        if len(self.ist)==0:
+            return False
+        else:
+            return self.ist.pop(0)
+    def is_empty(self):
+        return len(self.ist)==0
+    def size(self):
+        return len(self.ist)
+    def display(self):
+        return str(self.ist)
+x=Queue()
+x.enqueue("Asas")
+x.enqueue("Lia")
+print(x.dequeue())
+print(x.is_empty())
+print(x.size())
+print(x.display())
